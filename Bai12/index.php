654df08a1b17694710f444b5b28ce83ffa56de6d
Imelda.tp <100985589+thuphuong59@users.noreply.github.com>
2022-09-16 23:24:37 +0700

    Add files via upload

---
+++ Bai12/index.php
@@ -0,0 +1,22 @@
+
+<?php 
+include("functions.php");
+
+if(isset($_GET['id'])){
+	$id=$_GET['id'];
+	// UPDATE RATING
+	if(isset($_POST['rate_submit'])){
+		if(isset($_POST['rate'])){
+			$rate = $_POST['rate'];
+			setRatingInfo($id, $rate);
+		}
+	}
+
+	$html = getProduct($id);
+	include("view-product.php");
+}else{
+	$html = listProduct();
+	include("list-product.php");
+}
+
+?>
